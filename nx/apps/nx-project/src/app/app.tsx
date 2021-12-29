028ed2578cdf666a90aea920c531f4d810e86443
Harry Ji <jiharry@hotmail.com>
2021-12-28 17:15:03 -0800

    Tue 28 Dec 2021 05:15:03 PM PST

---
+++ nx/apps/nx-project/src/app/app.tsx
@@ -0,0 +1,13 @@
+import styles from './app.module.scss';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="nx-project" />
+      <div />
+    </>
+  );
+}
+
+export default App;
